0991da7f04ae3de92680a5ee0045f6920b35b77f
xnat <xnatural@msn.cn>
2022-02-19 15:22:56 +0800

    feat: CacheSrv 过期函数

--- src/main/java/cn/xnatural/app/CacheSrv.java
+++ src/main/java/cn/xnatural/app/CacheSrv.java
@@ -6,11 +6,13 @@ import java.time.Duration;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 
 /**
  * 简单内存缓存服务
  */
 public class CacheSrv extends ServerTpl {
+    // 触发清理多余数据的条件
     protected final Lazier<Integer> _limit = new Lazier(() -> getInteger("itemLimit", 1000));
 
     /**
@@ -44,59 +46,25 @@ public class CacheSrv extends ServerTpl {
     @EL(name = "{name}.set")
     public CacheSrv set(String key, Object value, Duration expire) {
         log.trace("Set cache. key: {}, value: {}, expire: {}", key, value, expire);
-        _data.get().put(key, new Record(expire == null ? null : expire.toMillis(), value));
-        boolean onlyCleanExpired = _data.get().size() < _limit.get(); // 是否只清理已过期的数据
-        Runnable clean = () -> {
-            Map.Entry<String, Record> oldEntry = null; // 最老的缓存
-            long oldLeft = 0;
-            for (Iterator<Map.Entry<String, Record>> iter = _data.get().entrySet().iterator(); iter.hasNext(); ) { //遍历选出最应该被移出的缓存记录
-                Map.Entry<String, Record> entry = iter.next();
-                long left = entry.getValue().left();
-                if (left < 1) { // 当前缓存记录已经失效了,删除
-                    iter.remove(); oldEntry = null; break;
-                }
-                if (onlyCleanExpired) continue;
-                // 优先清理过期时间越近的
-                if (
-                        oldEntry == null ||
-                        left < oldLeft ||
-                        (oldLeft == left && oldEntry.getValue().updateTime < entry.getValue().updateTime)
-                ) {
-                    oldEntry = entry;
-                    oldLeft = left;
-                }
-            }
-            if (oldEntry != null) {
-                _data.get().remove(oldEntry.getKey());
-            }
-        };
-        if (onlyCleanExpired) async(clean); // 异步清除多余的缓存
-        else clean.run(); // 同步清理: 避免异步排对太多而不能及时清理造成内存占用过多而溢出
+        _data.get().put(key, new Record(record -> expire == null ? Long.MAX_VALUE : expire.toMillis() + record.updateTime, value));
+        clean();
         return this;
     }
 
 
     /**
-     * 重新更新过期时间
+     * 设置缓存
      * @param key 缓存key
-     * @param expire 过期时间
-     * @return 缓存值
+     * @param value 缓存值
+     * @param expireFn 过期时间计算函数
+     *                 函数返回过期时间点(时间缀), 返回null(不过期,除非达到缓存限制被删除)
      */
-    @EL(name = "{name}.expire")
-    public Object expire(String key, Duration expire) {
-        Record record = _data.get().get(key);
-        if (record != null) {
-            if (expire != null) {
-                record.expire = expire.toMillis();
-                record.updateTime = System.currentTimeMillis();
-                log.debug("Updated cache: {}, expire: {}", key, expire);
-            } else {
-                _data.get().remove(key);
-                log.debug("Removed cache: {}", key);
-            }
-            return record.value;
-        }
-        return null;
+    @EL(name = "{name}.set2")
+    public CacheSrv set(String key, Object value, Function<Record, Long> expireFn) {
+        log.trace("Set cache. key: {}, value: {}", key, value);
+        _data.get().put(key, new Record(expireFn, value));
+        clean();
+        return this;
     }
 
 
@@ -106,7 +74,11 @@ public class CacheSrv extends ServerTpl {
      * @return 缓存值
      */
     @EL(name = "{name}.remove")
-    public Object remove(String key) { return expire(key, null); }
+    public Object remove(String key) {
+        Record record = _data.get().remove(key);
+        if (record == null) return null;
+        return record.value;
+    }
 
 
     /**
@@ -124,6 +96,63 @@ public class CacheSrv extends ServerTpl {
     }
 
 
+    /**
+     * 获取缓存值, 并更新缓存时间(即从现在开始重新计算过期时间)
+     * @param key 缓存key
+     * @return 缓存值
+     */
+    @EL(name = "{name}.getAndUpdate")
+    public Object getAndUpdate(String key) {
+        Record record = _data.get().get(key);
+        if (record == null) return null;
+        if (record.valid()) {
+            record.updateTime = System.currentTimeMillis();
+            return record.value;
+        }
+        _data.get().remove(key);
+        return null;
+    }
+
+
+    /**
+     * 清理过期和多余的数据
+     * 一次最多清一条
+     * 1. 如果没有达到缓存限制, 则只清理过期数据
+     * 2. 优先清理过期时间越近的
+     * 3. 过期时间一样则先清更新时间最早的
+     */
+    protected void clean() {
+        boolean onlyCleanExpired = _data.get().size() < _limit.get(); // 是否只清理已过期的数据
+        final Runnable clean = () -> {
+            Map.Entry<String, Record> oldEntry = null; // 最老的缓存
+            long oldLeft = 0;
+            long now = System.currentTimeMillis();
+            for (Iterator<Map.Entry<String, Record>> iter = _data.get().entrySet().iterator(); iter.hasNext(); ) { //遍历选出最应该被移出的缓存记录
+                Map.Entry<String, Record> entry = iter.next();
+                long left = entry.getValue().left(now);
+                if (left < 1) { // 过期数据
+                    iter.remove(); oldEntry = null; break;
+                }
+                if (onlyCleanExpired) continue;
+
+                if (
+                        oldEntry == null ||
+                        left < oldLeft ||
+                        (oldLeft == left && oldEntry.getValue().updateTime < entry.getValue().updateTime)
+                ) {
+                    oldEntry = entry;
+                    oldLeft = left;
+                }
+            }
+            if (oldEntry != null) {
+                _data.get().remove(oldEntry.getKey());
+            }
+        };
+        if (onlyCleanExpired) queue(clean); // 异步清除多余的缓存
+        else clean.run(); // 同步清理: 避免异步排对太多而不能及时清理造成内存占用过多而溢出
+    }
+
+
     @Override
     public String toString() {
         return "CacheSrv@" + Integer.toHexString(hashCode()) + "[size=" + _data.get().size() + ", limit=" + _limit.get() + "]";
@@ -133,27 +162,33 @@ public class CacheSrv extends ServerTpl {
     /**
      * 缓存记录
      */
-    protected class Record {
-        // null 或者 < 1, 都是永不过期
-        Long     expire;
+    public class Record {
+        // 过期时间点计算函数, 小于当前时间即过期, 返回null不过期
+        protected final Function<Record, Long> expireFn;
         // 更新时间
-        Long     updateTime = System.currentTimeMillis();
-        // 缓存值
-        Object   value;
+        protected long updateTime = System.currentTimeMillis();
+        // 缓存的对象
+        public final Object value;
 
-        public Record(Long expire, Object value) {
-            this.expire = expire;
+        protected Record(Function<Record, Long> expireFn, Object value) {
+            this.expireFn = expireFn;
             this.value = value;
         }
 
         /**
          * 是否有效
          */
-        boolean valid() { return left() > 0; }
+        protected boolean valid() { return left(System.currentTimeMillis()) > 0; }
 
         /**
-         * 剩多长时间才失效
+         * 相对于某个时间, 还剩多长时间才失效
          */
-        long left() { return (expire == null || expire < 1) ? Long.MAX_VALUE : ((updateTime + expire) - System.currentTimeMillis()); }
+        protected long left(long timePoint) {
+            Long expireTime = expireFn == null ? null : expireFn.apply(this);
+            return expireTime == null ? Long.MAX_VALUE : timePoint - expireTime;
+        }
+
+
+        public long getUpdateTime() { return updateTime; }
     }
 }
